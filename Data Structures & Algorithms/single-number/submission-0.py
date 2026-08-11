4147d984d28e4459f9c9f5f531c32a9bd75b7804
Justin Stevenson Theodorus <justin.theodorus@gmail.com>
2026-08-11 14:14:21 +0800

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        res = nums[0]
+
+        for i in range(1, n):
+            res ^= nums[i]
+        
+        return res
+"""
+XOR the same element cancels out (0) regardless of position
+"""
